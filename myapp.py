b88885252552dd15e46034afb66fc8814851749c
newlcj93 <362981473@qq.com>
2016-05-08 14:46:46 +0800

    update

--- myapp.py
+++ myapp.py
@@ -104,5 +104,5 @@ def wechat_auth():
         return response
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 443))
-    app.run(host='127.0.0.1', debug=True)
+    app.run(host='0.0.0.0',port=port,debug=True)
 
